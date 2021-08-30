c6a23a5608b843686a7c007bced935b25bf7c54d
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 15:54:43 +0530

    Add project files.

---
+++ ConsoleAppEnumeration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    class Program
+    {
+        enum MeetingDemo
+        {
+            Trivial,
+            Regular,
+            Critical,
+        }
+        static void Main(string[] args)
+        {
+            MeetingDemo meet = MeetingDemo.Critical;
+            int value = (int)MeetingDemo.Critical;
+
+            if (meet == MeetingDemo.Critical)
+            {
+                Console.WriteLine("Critical " + value);
+            }
+            else if (meet == MeetingDemo.Trivial)
+            {
+                Console.WriteLine("Trivial " + value);
+            }
+
+            else if (meet == MeetingDemo.Regular)
+            {
+                Console.WriteLine("Regular " + value);
+                Console.ReadKey();
+            }
+        }
+    }
+}
